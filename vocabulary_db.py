e05dfdec01a4bbe3fb450e937972ef5391932d8e
xswzaqnjimko <35910992+xswzaqnjimko@users.noreply.github.com>
2026-01-29 08:40:09 -0800

    Update vocabulary_db.py

--- vocabulary_db.py
+++ vocabulary_db.py
@@ -1,25 +1,5 @@
 # lingo-text-of-own: Language Learning from Literature - Vocabulary Database Module
 # SQLite database operations for vocabulary tracking
-#
-# Copyright (C) 2025 xswzaqnjimko
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# Documentation and comments in this file are licensed under CC BY 4.0.
-# See LICENSE-DOCS for details.
-
-# Currently (as of Nov. 2025) Chinese UI; English version to be incorporated.
 
 # 腿肉机翻生词本 - 数据库操作模块
 # 使用 SQLite 存储生词和遭遇历史
