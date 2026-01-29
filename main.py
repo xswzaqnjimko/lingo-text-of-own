20716858b07cac02c3ac5ce5c59e6b3d39443c15
xswzaqnjimko <35910992+xswzaqnjimko@users.noreply.github.com>
2026-01-29 08:38:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
 # lingo-text-of-own: Language Learning from User-Defined Literature Library
-# Learn foreign languages through authentic literary works
-# 
-# Copyright (C) 2025 xswzaqnjimko
-#
-# This program is a free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# Documentation and comments in this file are licensed under CC BY 4.0.
-# See LICENSE-DOCS for details.
-# 
-# Original development aided by AI assistants (GPT-5, Claude Sonnet 4.5)
 
 import os
 import re
